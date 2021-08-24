4ce734d9503956baec505abe1fa2fb91985c72a5
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-08-23 19:41:55 -0700

    first shell that acepts command prompt and back output

--- shell.c
+++ shell.c
@@ -12,6 +12,16 @@
  * Return: 0
  */
 
+void print_prompt1(void)
+{
+	fprintf(stderr, "$ ");
+}
+
+void print_prompt2(void)
+{
+	fprintf(stderr, "> ");
+}
+
 int main(int argc, char**argv)
 {
 	char *cmd;
@@ -50,4 +60,54 @@ int main(int argc, char**argv)
 	} while(1);
 
 	return (0);
+}
+
+char *read_cmd(void)
+{
+	char buf[1024];
+	char *ptr = NULL;
+	char ptrlen = 0;
+
+	while(fgets(buf, 1024, stdin))
+	{
+		int buflen = strlen(buf);
+
+		if(!ptr)
+		{
+			ptr = malloc(buflen + 1);
+		}
+		else
+		{
+			char *ptr2 = realloc(ptr, ptrlen + buflen + 1);
+			if(ptr2)
+			{
+				ptr = ptr2;
+			}
+			else
+			{
+				free(ptr);
+				ptr = NULL;
+			}
+		}
+		if(!ptr)
+		{
+			fprintf(stderr, "error: failed to alloc buffer: %s\n", strerror(errno));
+			return NULL;
+		}
+		strcpy(ptr + ptrlen, buf);
+
+		if(buf[buflen - 1] == '\n')
+		{
+			if(buflen == 1 || buf[buflen - 2] != '\\')
+			{
+				return ptr;
+			}
+
+			ptr[ptrlen + buflen - 2] = '\0';
+			buflen -= 2;
+			print_prompt2();
+		}
+		ptrlen += buflen;
+	}
+	return ptr;
 }
