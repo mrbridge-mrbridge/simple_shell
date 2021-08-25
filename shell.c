f70a884000715e0356e0f943ff89f799e433629b
mrbridge-mrbridge <nanamcroj@gmail.com>
2021-08-24 20:42:13 -0700

    update shell.c

--- shell.c
+++ shell.c
@@ -3,6 +3,9 @@
 #include <errno.h>
 #include <string.h>
 #include "shell.h"
+#include "source.h"
+#include "parser.h"
+#include "backend.h"
 
 /*
  * main - simple shell
@@ -55,7 +58,12 @@ int main(int argc, char**argv)
 			break;
 		}
 
-		printf("%s\n", cmd);
+		struct source_s src;
+		src.buffer = cmd;
+		src.bufsize = strlen(cmd);
+		src.curpos = INIT_SRC_POS;
+		parse_and_execute(&src);
+
 		free(cmd);
 	} while(1);
 
@@ -110,4 +118,30 @@ char *read_cmd(void)
 		ptrlen += buflen;
 	}
 	return ptr;
+}
+
+int parse_and_execute(struct source_s *src)
+{
+	skip_white_spaces(src);
+
+	struct token_s *tok = tokenize(src);
+
+	if(tok == &eof_token)
+	{
+		return (0);
+	}
+	while(tok && tok != &eof_token)
+	{
+		struct node_s *cmd = parse_simple_command(tok);
+		if(!cmd)
+		{
+			break;
+		}
+
+		do_simple_command(cmd);
+		free_node_tree(cmd);
+		tok = tokenize(src);
+	}
+
+	return (1);
 }
